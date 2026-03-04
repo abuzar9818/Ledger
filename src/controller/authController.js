e17334497eeb8374523e2403187a05a7567e4b6d
abuzar9818 <abuzarahmad678@gmail.com>
2026-03-05 01:54:10 +0530

    Implement user login functionality and update auth routes

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,6 +1,8 @@
 const userModel=require('../models/userModel');
 const jwt=require('jsonwebtoken');
 
+//user registration controller
+// Post api/auth/register
 async function userRegisterController(req,res){
     const {name,email,password}=req.body;
 
@@ -24,4 +26,33 @@ async function userRegisterController(req,res){
         token});
 }
 
-module.exports={userRegisterController};
+//user login controller
+// Post api/auth/login
+async function userLoginController(req,res){
+    const {email,password}=req.body;
+
+    const user=await userModel.findOne({email}).select('+password');
+    if(!user){
+        return res.status(404).json({message:"User not found",status:"failed"});
+    }
+
+    const isMatch=await user.comparePassword(password);
+    if(!isMatch){
+        return res.status(401).json({message:"Invalid credentials",status:"failed"});
+    }
+
+    const token=jwt.sign({userId:user._id},process.env.JWT_SECRET_KEY,{expiresIn:'3d'});
+
+    res.cookie('token',token)
+    res.status(200).json({
+        user:{
+            _id:user._id,
+            name:user.name,
+            email:user.email
+        },
+        message:"User logged in successfully",
+        status:"success",
+        token});
+}
+
+module.exports={userRegisterController,userLoginController};
