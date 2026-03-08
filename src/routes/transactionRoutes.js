c797020b15fe40011d6b8408ac4970a7d823b9bb
abuzar9818 <abuzarahmad678@gmail.com>
2026-03-08 09:16:19 +0530

    Add transaction creation controller to transaction routes

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,9 +1,10 @@
 const {Router}=require('express');
 const authMiddleware=require('../middleware/authMiddleware');
+const transactionContoller=require('../controller/transactionController');
 
 const transactionRoutes=Router();
 
 //Post /api/transactions - Create a new transaction
-transactionRoutes.post('/',authMiddleware.authMiddleware);
+transactionRoutes.post('/',authMiddleware.authMiddleware, transactionContoller.createTransactionController);
 
 module.exports=transactionRoutes;
