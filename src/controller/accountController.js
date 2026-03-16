a962f9635c59dc06d9805e67e8ef1e8a9284c862
abuzar9818 <abuzarahmad678@gmail.com>
2026-03-17 00:32:40 +0530

    Add GET route for retrieving account balance by account ID

--- src/controller/accountController.js
+++ src/controller/accountController.js
@@ -23,4 +23,26 @@ async function getUserAccountsController(req, res) {
     });
 }
 
-module.exports = { createAccountController, getUserAccountsController };
+// Get account balance by account ID
+async function getAccountBalanceController(req, res) {
+    const {accountId} = req.params;
+    
+    const account = await accountModel.findOne({ 
+        _id: accountId, 
+        user: req.user._id 
+    });
+
+    if (!account) {
+        return res.status(404).json({ message: 'Account not found' });
+    }
+   
+    const balance = await account.getBalance();
+
+    res.status(200).json({
+        accountId,
+        balance
+    });
+
+}
+
+module.exports = { createAccountController, getUserAccountsController, getAccountBalanceController };
