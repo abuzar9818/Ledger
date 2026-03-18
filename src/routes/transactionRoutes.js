918c08e82e71f50dbfd6b826153992d1dc97a3f5
abuzar9818 <abuzarahmad678@gmail.com>
2026-03-19 02:47:06 +0530

    Add GET route to retrieve all transactions for authenticated users

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -10,4 +10,8 @@ transactionRoutes.post('/',authMiddleware.authMiddleware, transactionContoller.c
 //Post /api/transactions/system/initial-fund
 transactionRoutes.post('/system/initial-fund',authMiddleware.systemUserMiddleware, transactionContoller.createInitialFundController);
 
+//Get /api/transactions - Get all transactions for the authenticated user
+transactionRoutes.get('/',authMiddleware.authMiddleware, transactionContoller.getTransactionsController);
+
+
 module.exports=transactionRoutes;
