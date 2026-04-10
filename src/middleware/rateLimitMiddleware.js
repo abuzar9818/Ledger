6f9863a47c08abc32736a70af472cb865da0b2f9
abuzar9818 <abuzarahmad678@gmail.com>
2026-04-10 12:46:45 +0530

    Refactor rate limit middleware to improve key generation and enhance code readability

--- src/middleware/rateLimitMiddleware.js
+++ src/middleware/rateLimitMiddleware.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const rateLimit = require('express-rate-limit');
+const { ipKeyGenerator } = require('express-rate-limit'); 
 
 const ONE_MINUTE_MS = 60 * 1000;
+
 const MAX_REQUESTS_PER_MINUTE =
     process.env.MAX_REQUESTS_PER_MINUTE || 20;
+
 const LOGIN_RATE_LIMIT =
     process.env.LOGIN_RATE_LIMIT || 5;
+
 const TRANSACTION_RATE_LIMIT =
     process.env.TRANSACTION_RATE_LIMIT || 5;
+
 
 function jsonRateLimitHandler(req, res) {
     return res.status(429).json({
@@ -16,27 +21,33 @@ function jsonRateLimitHandler(req, res) {
     });
 }
 
+
 function createLimiter(maxRequests = MAX_REQUESTS_PER_MINUTE) {
     return rateLimit({
         windowMs: ONE_MINUTE_MS,
         max: maxRequests,
+
         skip: (req) => req.user?.role === "SYSTEM",
+
         keyGenerator: (req) => {
             if (req.user) {
                 return req.user._id.toString();
             }
-            // Use the ipKeyGenerator to properly handle IPv6
-            return rateLimit.ipKeyGenerator(req);
+
+            return ipKeyGenerator(req);
         },
+
         standardHeaders: true,
         legacyHeaders: false,
         handler: jsonRateLimitHandler
     });
 }
 
-const authLimiter = createLimiter(LOGIN_RATE_LIMIT); // stricter limit for auth routes to prevent brute-force attacks
-const transactionLimiter = createLimiter(TRANSACTION_RATE_LIMIT); // safer limit for transaction-related routes to prevent abuse
-const generalLimiter = createLimiter(MAX_REQUESTS_PER_MINUTE); // default limit for general routes
+
+const authLimiter = createLimiter(LOGIN_RATE_LIMIT);
+const transactionLimiter = createLimiter(TRANSACTION_RATE_LIMIT);
+const generalLimiter = createLimiter(MAX_REQUESTS_PER_MINUTE);
+
 
 module.exports = {
     authLimiter,
