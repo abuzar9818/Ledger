97bb570f36ca6a1ee42f6abb6514fe1acc9c61ac
abuzar9818 <abuzarahmad678@gmail.com>
2026-04-19 01:54:51 +0530

    Add initial app.js file with Express setup and route imports

---
+++ Backend/app.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const morgan=require('morgan');
+const cookieParser=require('cookie-parser');
+const swaggerUi=require('swagger-ui-express');
+
+// Import routes
+const authRoutes=require('./routes/authRoutes');
+const accountRoutes=require('./routes/accountRoutes');
+const transactionRoutes=require('./routes/transactionRoutes');
+const scheduledTransactionRoutes=require('./routes/scheduledTransactionRoutes');
+const reportsRoutes=require('./routes/reportsRoutes');
+const freezeUnfreezeRoutes=require('./routes/Freeze_UnfreezeRoutes');
+const adminRoutes=require('./routes/adminRoutes');
+const accountClosureRequestRoutes=require('./routes/accountClosureRequestRoutes');
+const accountReopenRequestRoutes=require('./routes/accountReopenRequestRoutes');
+const swaggerDocument=require('./config/swagger');
+
+const app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+// Use routes
+
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Ledger API");
+});
+
+app.use('/api/auth',authRoutes);
+app.use('/api/accounts',accountRoutes);
+app.use('/api/transactions',transactionRoutes);
+app.use('/api/scheduled-transactions',scheduledTransactionRoutes);
+app.use('/reports',reportsRoutes);
+app.use('/api',freezeUnfreezeRoutes);
+app.use('/admin',adminRoutes);
+app.use('/api/account-closure-requests',accountClosureRequestRoutes);
+app.use('/api/account-reopen-requests',accountReopenRequestRoutes);
+
+
+module.exports=app;
