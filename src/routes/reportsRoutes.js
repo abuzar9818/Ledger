e4f41ab357ae180ee39be0412099ffdad3b0d87e
abuzar9818 <abuzarahmad678@gmail.com>
2026-04-19 01:58:12 +0530

    Remove unused route files and scheduled transaction service

--- src/routes/reportsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authMiddleware = require('../middleware/authMiddleware');
-const reportsController = require('../controller/reportsController');
-
-const router = express.Router();
-
-router.get('/monthly-summary', authMiddleware.authMiddleware, reportsController.getMonthlySummary);
-
-module.exports = router;
