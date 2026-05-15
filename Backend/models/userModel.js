eb6f478f6247843e69ca73950c8f6c05f1f1f59e
abuzar9818 <abuzarahmad678@gmail.com>
2026-05-15 22:35:21 +0530

    fix: update email regex validation patterns to ensure consistency across backend and frontend

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -7,7 +7,7 @@ const userSchema=new mongoose.Schema({
         required:[true,"Email is required"],
         trim:true,
         lowercase:true,
-        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Please fill a valid email address"],
+        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,"Please fill a valid email address"],
         unique:[true,"Email already exists"]
     },
     name:{
